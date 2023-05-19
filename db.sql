fc01dca9e7bbfc483d64b81f054ca96424d35cd9
Cody <cjoel91@hotmail.com>
2023-05-18 19:03:21 -0600

    Updates dbtablesrelationships

---
+++ db.sql
@@ -0,0 +1,118 @@
+-- MariaDB dump 10.19  Distrib 10.6.13-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: db_table_relationships
+-- ------------------------------------------------------
+-- Server version	10.6.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `blog_post`
+--
+
+DROP TABLE IF EXISTS `blog_post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `blogger_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `blogger_id` (`blogger_id`),
+  CONSTRAINT `blog_post_ibfk_1` FOREIGN KEY (`blogger_id`) REFERENCES `blogger` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_post`
+--
+
+LOCK TABLES `blog_post` WRITE;
+/*!40000 ALTER TABLE `blog_post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blog_post_comment`
+--
+
+DROP TABLE IF EXISTS `blog_post_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blog_post_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `blogger_id` int(11) DEFAULT NULL,
+  `blog_post_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `blogger_id` (`blogger_id`),
+  KEY `blog_post_id` (`blog_post_id`),
+  CONSTRAINT `blog_post_comment_ibfk_1` FOREIGN KEY (`blogger_id`) REFERENCES `blogger` (`id`),
+  CONSTRAINT `blog_post_comment_ibfk_2` FOREIGN KEY (`blog_post_id`) REFERENCES `blog_post` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog_post_comment`
+--
+
+LOCK TABLES `blog_post_comment` WRITE;
+/*!40000 ALTER TABLE `blog_post_comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blog_post_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blogger`
+--
+
+DROP TABLE IF EXISTS `blogger`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blogger` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blogger`
+--
+
+LOCK TABLES `blogger` WRITE;
+/*!40000 ALTER TABLE `blogger` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blogger` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_table_relationships'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-18 19:03:03
